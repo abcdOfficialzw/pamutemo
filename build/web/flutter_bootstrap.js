cb2c1a72ff4c70d6157beddd07ccd981bfcbbe62
Takudzwa Nyanhanga <takutitus09@gmail.com>
2026-04-16 18:35:43 +0200

    chore: 🔧 Update service worker and resources versions chore: 🔧 Change domain in URLs to pamutemo-app.takutitus.me

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1691295468"
+    serviceWorkerVersion: "3994172972"
   }
 });
